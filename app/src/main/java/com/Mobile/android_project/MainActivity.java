5264af21be415dfe5f251dea8942ab2e7b5e0789
1m1nkim <window12ty@naver.com>
2023-05-17 20:54:42 +0900

    비밀번호 구현

--- app/src/main/java/com/Mobile/android_project/MainActivity.java
+++ app/src/main/java/com/Mobile/android_project/MainActivity.java
@@ -23,10 +23,11 @@ public class MainActivity extends AppCompatActivity {
     Dialog findDialog_p;
     Dialog findDialognum_g;
     Dialog findDialognum_b;
-    Dialog findDialogpass_g;
+    Dialog changePass;
+    Dialog passDialogb;
+    Dialog change_good;
+    Dialog notsameDialog;
     boolean check_cb = false;
-    boolean check_edit_id = false;
-    boolean check_edit_birth = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,8 +40,6 @@ public class MainActivity extends AppCompatActivity {
         Button login_btn = (Button) findViewById(R.id.login_btn);
         TextView failText = (TextView) findViewById(R.id.login_fail_text);
         Button auto_login_fill = (Button) findViewById(R.id.auto_login_fill);
-        EditText edit_id = (EditText) findViewById(R.id.edit_id);
-        EditText edit_birth = (EditText) findViewById(R.id.edit_birth);
 
 
         findDialog = new Dialog(MainActivity.this);
@@ -58,6 +57,23 @@ public class MainActivity extends AppCompatActivity {
         findDialognum_b = new Dialog(MainActivity.this);
         findDialognum_b.requestWindowFeature(Window.FEATURE_NO_TITLE);
         findDialognum_b.setContentView(R.layout.findnum_bad);
+
+        changePass = new Dialog(MainActivity.this);
+        changePass.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        changePass.setContentView(R.layout.find_pass_good);
+
+        passDialogb = new Dialog(MainActivity.this);
+        passDialogb.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        passDialogb.setContentView(R.layout.find_pass_bad);
+
+
+        change_good = new Dialog(MainActivity.this);
+        change_good.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        change_good.setContentView(R.layout.change_pass_good);
+
+        notsameDialog = new Dialog(MainActivity.this);
+        notsameDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        notsameDialog.setContentView(R.layout.notsamepass);
 
         failText.setVisibility(View.INVISIBLE);
         login_btn.setEnabled(false);
@@ -80,6 +96,7 @@ public class MainActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
             }
         });
+        //--------------------------로그인 버튼-----------------------------
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -102,27 +119,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        //--------------------------찾기 버튼-----------------------------
         find_id.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 String name = "김한민";
-                System.out.println(name + "-----------------------------");
                 String birthh = "001120";
                 findDialog.show(); // 다이얼로그 띄우기
 
                 EditText edit_num = (EditText) findDialog.findViewById(R.id.edit_id);
                 EditText edit_birth = (EditText) findDialog.findViewById(R.id.edit_birth);
+
                 edit_num.setText("");
                 edit_birth.setText("");
+                //--------------------------찾기-확인 버튼-----------------------------
                 Button submit_btn = findDialog.findViewById(R.id.submit_btn);
-                submit_btn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        String edit_Num = edit_num.getText().toString();
-                        String edit_Birth = edit_birth.getText().toString();
-
+                        submit_btn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                String edit_Num = edit_num.getText().toString();
+                                String edit_Birth = edit_birth.getText().toString();
                         if(edit_Num.equals(name) && edit_Birth.equals(birthh)){
                             findDialog.dismiss();
                             findDialognum_g.show();
@@ -133,6 +150,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+                //--------------------------찾기-찾았을경우-확인 버튼-----------------------------
                 Button submit_btn_ng = findDialognum_g.findViewById(R.id.submit_btn_ng);
                 submit_btn_ng.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -140,6 +158,7 @@ public class MainActivity extends AppCompatActivity {
                         findDialognum_g.dismiss();// 다이얼로그 닫기
                     }
                 });
+                //--------------------------찾기-못찾았을경우-확인 버튼-----------------------------
                 Button submit_btn_nb = findDialognum_b.findViewById(R.id.submit_btn_nb);
                 submit_btn_nb.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -147,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
                         findDialognum_b.dismiss();// 다이얼로그 닫기
                     }
                 });
+                //--------------------------취소 버튼-----------------------------
                 Button noBtn = findDialog.findViewById(R.id.cancel_btn);
                 noBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -156,10 +176,112 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+//------------------------------------------------------------------------------------------------------------------------------------------------------
         find_pass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDialog_pass();
+
+                String p_name = "김한민";
+                String p_hak = "2019081070";
+                findDialog_p.show(); // 다이얼로그 띄우기
+
+                EditText edit_num_p = (EditText) findDialog_p.findViewById(R.id.edit_id_p);
+                EditText edit_hak_p = (EditText) findDialog_p.findViewById(R.id.edit_birth_p);
+
+                edit_num_p.setText("");
+                edit_hak_p.setText("");
+
+
+                //--------------------------찾기-확인 버튼-----------------------------
+                Button submit_btn_p = findDialog_p.findViewById(R.id.submit_btn_p);
+                submit_btn_p.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String edit_Num_p = edit_num_p.getText().toString();
+                        String edit_Birth_p = edit_hak_p.getText().toString();
+                        if(edit_Num_p.equals(p_name) && edit_Birth_p.equals(p_hak)){
+                            findDialog_p.dismiss();
+                            changePass.show();
+                        }else{
+                            edit_num_p.setText("");
+                            edit_hak_p.setText("");
+                            passDialogb.show();
+                        }
+                    }
+                });
+                //--------------------------찾기-취소 버튼-----------------------------
+                Button cancel_Btn_p = findDialog_p.findViewById(R.id.cancel_btn_p);
+                cancel_Btn_p.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        findDialog_p.dismiss();
+                    }
+                });
+                //////////////////////////////////////////////////////////////////////
+
+
+                Button submit_btn_nb = passDialogb.findViewById(R.id.submit_btn_nb);
+                submit_btn_nb.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        passDialogb.dismiss();
+                    }
+                });
+
+                Button submit_btn_ng = notsameDialog.findViewById(R.id.submit_btn_ng);
+                submit_btn_ng.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        notsameDialog.dismiss();
+                    }
+                });
+                //--------------------------바꾼 후 확인 버튼------------------------------
+                EditText ch_pass = changePass.findViewById(R.id.ch_pass);
+                EditText ch_pass_check = changePass.findViewById(R.id.ch_pass_check);
+                ch_pass.setText("");
+                ch_pass_check.setText("");
+                Button submit_btn_c = changePass.findViewById(R.id.submit_btn_c);
+                submit_btn_c.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        String ch_Pass = ch_pass.getText().toString();
+                        String ch_Pass_check = ch_pass_check.getText().toString();
+                        System.out.println(ch_Pass + ch_Pass_check + "-----------------------------------------------------------------------");
+                        if(ch_Pass.equals(ch_Pass_check)){
+                            changePass.dismiss();
+                            change_good.show();
+                        }else{
+                            ch_pass.setText("");
+                            ch_pass_check.setText("");
+                            notsameDialog.show();
+                        }
+                    }
+                });
+
+                //--------------------------바꾸기 취소 버튼-----------------------------
+                Button ch_nobtn = changePass.findViewById(R.id.cancel_btn_c);
+                ch_nobtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        changePass.dismiss();// 다이얼로그 닫기
+                    }
+                });
+                //--------------------------changePAss취소 버튼-----------------------------
+                Button noBtn_p = changePass.findViewById(R.id.cancel_btn_c);
+                noBtn_p.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        changePass.dismiss();// 다이얼로그 닫기
+                    }
+                });
+                Button submit_btn_pass = change_good.findViewById(R.id.submit_btn_pass);
+                submit_btn_pass.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        change_good.dismiss();
+                    }
+                });
             }
         });
         auto_login_fill.setOnClickListener(new View.OnClickListener() {
@@ -174,19 +296,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-
-
-    public void showDialog_pass() {
-        findDialog_p.show(); // 다이얼로그 띄우기
-
-        Button noBtn = findDialog_p.findViewById(R.id.cancel_btn_p);
-        noBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                findDialog_p.dismiss();// 다이얼로그 닫기
-            }
-        });
-
     }
 }
